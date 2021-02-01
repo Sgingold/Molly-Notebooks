f736ef1c077bb02b61309ca3e785e401fae397d8
mollybair <mollybair@users.noreply.github.com>
2021-02-01 13:00:08 -0500

    basic shiny app

--- Week 4/Week 4 Complete Code.R
+++ Week 4/Week 4 Complete Code.R
@@ -11,9 +11,45 @@ spotify
 # Clean and subset data
 spotify$artists <- str_replace_all(spotify$artists, "['']", "")
 spotify$artists <- str_replace_all(spotify$artists, "\\[|\\]", "")
-keep = c("Fiona Apple", "Metallica", "Taylor Swift")
 artist_subset <- spotify %>%
-  filter(artists %in% keep)
+  filter(artists == "Fiona Apple")
 view(artist_subset)
 
+# Component 1: User Interface Object
+features <- c("danceability", "energy", "popularity")
+ui <- fluidPage(
+  titlePanel("Fiona Apple's Music Over the Years"),
+  
+  selectInput("feature", "Select an audio feature to display",
+              list("energy", "danceability",
+                   "tempo", "acousticness")
+              ),
+  textOutput("result"),
+  
+  plotOutput("scatter", click = "plot_click"),
+  
+  verbatimTextOutput("text")
+)
 
+# Component 2: Server Function
+server <- function(input, output) {
+  data <- reactive({
+    if ("energy" %in% input$feature) return (artist_subset$energy)
+    if ("danceability" %in% input$feature) return (artist_subset$danceability)
+    if ("tempo" %in% input$feature) return (artist_subset$tempo)
+    if ("acousticness" %in% input$feature) return (artist_subset$acousticness)
+  })
+  
+  output$scatter <- renderPlot({
+    plot(x = data(), y = artist_subset$year,
+    xlab = input$feature,
+    ylab = "Year")
+  })
+  
+  output$text <- renderText({
+    paste0("This song is titled ", input$plot_click$x)
+  })
+}
+
+# Component 3: Call shinyApp Function
+shinyApp(ui = ui, server = server)
