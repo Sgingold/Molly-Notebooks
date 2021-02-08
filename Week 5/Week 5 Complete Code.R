70aa740cdccb62fccbda72ed4fdbf3bf26476a06
mollybair <mollybair@users.noreply.github.com>
2021-02-08 18:17:50 -0500

    Part of speech analysis

--- Week 5/Week 5 Complete Code.R
+++ Week 5/Week 5 Complete Code.R
@@ -1,6 +1,8 @@
 ## Goal: Identify main subjects and sentiments of an unknown text
 # Text retrieved from https://www.theatlantic.com/health/archive/2021/02/uh-oh-spaghetti-os-pie/617949/
+rm(list=ls())
 
+library(plyr)
 library(tidyverse)
 library(tidytext)
 
@@ -10,12 +12,17 @@ my_text <- tibble(text = mystery_text)
 my_text
 
 # Prepare for analysis (tokenize, remove stop words, etc)
-text_words <- unnest_tokens(my_text, words, text, token = "words")
-text_words <- anti_join(text_words, stop_words, by = c("words" = "word"))
-text_words
+words_df <- unnest_tokens(my_text, words, text, token = "words")
+words_df <- anti_join(words_df, stop_words, by = c("words" = "word"))
+unique_words <- count(words_df, "words")
+view(unique_words)
 
 # Part of speech analysis
+words_pos <- left_join(unique_words, parts_of_speech, by = c("words" = "word"))
+view(words_pos)
 
 # Sentiment analysis
 
-# Visualize results
+
+
+
