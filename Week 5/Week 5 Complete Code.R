26a540d1407601522865be6b845d28df3d2e9fa6
mollybair <mollybair@users.noreply.github.com>
2021-02-08 17:27:52 -0500

    Libraries and read text

---
+++ Week 5/Week 5 Complete Code.R
@@ -0,0 +1,8 @@
+## Goal: Identify main subjects and sentiments of an unknown text
+# Text retrieved from https://www.theatlantic.com/health/archive/2021/02/uh-oh-spaghetti-os-pie/617949/
+
+library(tidyverse)
+library(tidytext)
+
+
+
