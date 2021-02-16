822dd505add7fcf3c57892e705099487bc5ca0ab
mollybair <mollybair@users.noreply.github.com>
2021-02-16 11:14:50 -0500

    topic model

--- Week 6/Week 6 Complete Code.R
+++ Week 6/Week 6 Complete Code.R
@@ -1,6 +1,6 @@
 # OBJECTIVE 1: Review relative file paths
 # OBJECTIVE 2: Create generalizable code for NLP, analyze multiple waves of text data, 
-#              perform sentiment analysis on specific tokens 
+#              perform topic modeling
 
 # Data downloaded from: https://fiscal.treasury.gov/reports-statements/financial-report/previous-reports.html
 
@@ -16,6 +16,10 @@ paste0(path, filename)
 library(tidyverse)
 library(tidytext)
 library(udpipe)
+library(topicmodels)
+library(igraph)
+library(ggraph)
+library(ggplot2)
 
 # Create function to read text files
 fnames <- list("fr2016.txt", "fr2015.txt", "fr2014.txt")
@@ -32,26 +36,43 @@ for (index in 1:length(fnames)) {
   parse_text(fnames[[index]], obj_names[[index]])
 }
 
-# Sentiment analysis of particular token
-token_sa <- function(token, sa_df, parsed_text, new_df_name) {
-  df <- parsed_text %>%
-    filter(token == token) %>%
-    select("token_id", "token", "lemma", "upos", "head_token_id", "dep_rel") %>%
-    inner_join(sa_df, by = c("token" = "word"))
-  assign(x = new_df_name, value = df, envir = globalenv())
-  return(new_df_name)
-}
-sentiment_bing <- get_sentiments("bing")
-parsed_dfs <- list(parsed16, parsed15, parsed14)
-sa_names <- list("ob16", "ob15", "ob14")
-for (index in 1:length(obj_names)) {
-  token_sa("Obama", sentiment_bing, parsed_dfs[[index]], sa_names[[index]])
-}
+# Topic modeling - on nouns
+# https://cran.r-project.org/web/packages/udpipe/vignettes/udpipe-usecase-topicmodelling.html
+# Prepare model
+ud_model <- udpipe_download_model(language = "english")
+ud_model <- udpipe_load_model(ud_model$file_model)
 
+# Prepare our parsed data by adding a topic modeling id
+parsed16$topic_model_id <- unique_identifier(parsed16, fields = c("doc_id", "paragraph_id", "sentence_id"))
+nouns16 <- parsed16 %>%
+  filter(upos == "NOUN") %>%
+  document_term_frequencies(document = "topic_model_id", term = "lemma")
+view(nouns16)
 
+# Create document/term/matrix
+dtm16 <- document_term_matrix(x = nouns16)
+dtm16_freq <- dtm_remove_lowfreq(dtm, minfreq = 2)
+head(dtm_colsums(dtm16_freq))
 
+# Create topic model
+model <- LDA(dtm16_freq, k = 4, method = "Gibbs", 
+             control = list(nstart = 5, burnin = 2000, best = TRUE, seed = 1:5))
 
+# Visualize model
+terms <- predict(model, type = "terms", min_posterior = 0.05, min_terms = 4)
+scores <- predict(model, newdata = dtm, type = "topics", 
+                  labels = c("topic1", "topic2", "topic3", "topic4"))
+topics <- merge(parsed16, scores, by.x = "topic_model_id", by.y = "doc_id")
 
+wordnetwork <- subset(topics, topic %in% 1 & lemma %in% terms[[1]]$term)
+wordnetwork <- cooccurrence(wordnetwork, group = c("topic_model_id"), term = "lemma")
+wordnetwork <- graph_from_data_frame(wordnetwork)
+
+ggraph(wordnetwork, layout = "fr") +
+  geom_edge_link(aes(width = cooc, edge_alpha = cooc), edge_colour = "pink")  +
+  geom_node_text(aes(label = name), col = "darkgreen", size = 4) +
+  labs(title = "Cooccurrence of Nouns in Topic 1") +
+  theme_void()
 
 
 
